b6f67bdc38f77e4385e78be6e83fc193941a70a8
onionwyl <onionwyl@qq.com>
2019-04-05 17:09:28 +0800

    Add multi-thread echo server

---
+++ echoserver_thread.c
@@ -0,0 +1,33 @@
+#include"server.h"
+#include<pthread.h>
+
+void *thread(void *vargp);
+
+int main(int argc, char **argv) {
+    int listenfd, *connfdp;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+    if(argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(0);
+    }
+    listenfd = open_listenfd(argv[1]);
+
+    while(1) {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfdp = malloc(sizeof(int));
+        *connfdp = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+        pthread_create(&tid, NULL, thread, connfdp);
+    }
+}
+
+void *thread(void *vargp) {
+    int connfd = *((int *)vargp);
+    pthread_detach(pthread_self());
+    free(vargp);
+    echo(connfd);
+    close(connfd);
+    return NULL;
+}
